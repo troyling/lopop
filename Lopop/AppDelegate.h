44ddae4f9b7ac12ee63ca11dd3a7b019b01efcce
Troy Ling <troyling14@gmail.com>
2015-01-09 23:09:49 -0500

    Initial Commit

---
+++ Lopop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lopop
+//
+//  Created by Troy Ling on 1/9/15.
+//  Copyright (c) 2015 Lopop Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lopop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lopop
+//
+//  Created by Troy Ling on 1/9/15.
+//  Copyright (c) 2015 Lopop Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
