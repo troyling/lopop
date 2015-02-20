0ff8986eafbef98de571bffcd0706d90800d2af8
Troy Ling <troyling14@gmail.com>
2015-02-19 21:38:38 -0500

    we know when user quit our app now

--- Lopop/AppDelegate.m
+++ Lopop/AppDelegate.m
@@ -74,6 +74,11 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    PFUser *currentUser = [PFUser currentUser];
+    if (currentUser) {
+        currentUser[@"lastSeenAt"] = [NSDate date];
+        [currentUser saveEventually];
+    }
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
