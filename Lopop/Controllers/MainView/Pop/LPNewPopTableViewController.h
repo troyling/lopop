faa0306839abe4fd44b07b1595341022cfc0d61b
Troy Ling <troyling14@gmail.com>
2015-03-25 12:28:23 -0400

    add dynamic address loading to new pop view

--- Lopop/Controllers/MainView/Pop/LPNewPopTableViewController.h
+++ Lopop/Controllers/MainView/Pop/LPNewPopTableViewController.h
@@ -11,7 +11,7 @@
 #import <CoreLocation/CoreLocation.h>
 #import <MobileCoreServices/MobileCoreServices.h>
 
-@interface LPNewPopTableViewController : UITableViewController <UIAlertViewDelegate, UITextFieldDelegate, UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate, CLLocationManagerDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate>
+@interface LPNewPopTableViewController : UITableViewController <UIAlertViewDelegate, UITextFieldDelegate, UIActionSheetDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate, CLLocationManagerDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate, MKMapViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *titleTextField;
 @property (weak, nonatomic) IBOutlet UILabel *categoryLabel;
@@ -26,6 +26,7 @@
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapview;
 @property (weak, nonatomic) IBOutlet UIVisualEffectView *mapBlurView;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
 
 - (IBAction)cancelNewPop:(id)sender;
 - (IBAction)addPhoto:(id)sender;
