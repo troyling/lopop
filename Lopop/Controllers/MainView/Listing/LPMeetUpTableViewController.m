409bc85e266422cc5b2a9ecce02b02b0088b41b0
Troy Ling <troyling14@gmail.com>
2015-05-17 01:55:59 -0400

    add layout for meetup

--- Lopop/Controllers/MainView/Listing/LPMeetUpTableViewController.m
+++ Lopop/Controllers/MainView/Listing/LPMeetUpTableViewController.m
@@ -7,13 +7,16 @@
 //
 
 #import "LPMeetUpTableViewController.h"
+#import "LPMeetUpTableViewCell.h"
+#import "UIImageView+WebCache.h"
+#import "LPLocationHelper.h"
 #import "LPOffer.h"
 #import "LPPop.h"
 
 @interface LPMeetUpTableViewController ()
 
-@property (strong, nonatomic) NSMutableArray *listingMeetUps;
-@property (strong, nonatomic) NSMutableArray *offerMeetUps;
+@property (strong, nonatomic) NSMutableArray *incomingOffers;
+@property (strong, nonatomic) NSMutableArray *myOffers;
 
 @end
 
@@ -25,9 +28,8 @@
 }
 
 - (void)loadData {
-    self.listingMeetUps = [[NSMutableArray alloc] init];
-    self.offerMeetUps = [[NSMutableArray alloc] init];
-    NSLog(@"WTF");
+    self.incomingOffers = [[NSMutableArray alloc] init];
+    self.myOffers = [[NSMutableArray alloc] init];
 
     // two types of meetups here
     // my selling meetup
@@ -39,10 +41,13 @@
 
     PFQuery *incomingOfferQuery = [LPOffer query];
     [incomingOfferQuery whereKey:@"status" equalTo:[NSNumber numberWithInt:status]];
+    [incomingOfferQuery includeKey:@"fromUser"];
+    [incomingOfferQuery includeKey:@"pop"];
     [incomingOfferQuery whereKey:@"pop" matchesQuery:myListingQuery];
     [incomingOfferQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         if (!error) {
-            NSLog(@"objectes are: \n%@", objects);
+            [self.incomingOffers addObjectsFromArray:objects];
+            [self.tableView reloadData];
         } else {
             NSLog(@"%@", error);
         }
@@ -56,12 +61,44 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    // Return the number of rows in the section.
-    return 0;
+    return self.incomingOffers == nil ? 0 : self.incomingOffers.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return nil;
+
+    NSString *identifier = @"meetUpCell";
+    LPMeetUpTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
+
+    if (!cell) {
+        cell = [[LPMeetUpTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+
+    // TODO show all offers
+    LPOffer *offer = [self.incomingOffers objectAtIndex:indexPath.row];
+
+    // Time
+    NSTimeZone *timeZoneLocal = [NSTimeZone localTimeZone];
+    NSDateFormatter *outputDateFormatter = [[NSDateFormatter alloc] init];
+    [outputDateFormatter setTimeZone:timeZoneLocal];
+    [outputDateFormatter setDateFormat:@"EEE, MMM d, h:mm a"];
+    NSString *outputString = [outputDateFormatter stringFromDate:offer.meetUpTime];
+    [cell.timeLabel setText:outputString];
+
+    // location
+    [LPLocationHelper getAddressForGeoPoint:offer.meetUpLocation withBlock: ^(NSString *address, NSError *error) {
+        if (!error) {
+            [cell.locationLabel setText:address];
+        }
+    }];
+
+    // user
+    [cell.profileImgView sd_setImageWithURL:offer.fromUser[@"profilePictureUrl"]];
+    cell.nameLabel.text = offer.fromUser[@"name"];
+
+    // pop info
+    cell.popTitleLabel.text = offer.pop.title;
+
+    return cell;
 }
 
 @end
